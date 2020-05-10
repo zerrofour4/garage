7fbc958f773713c2cd1cbe5a4207e9da179cc63b
Steve Olson <parmenio24@gmail.com>
2020-05-10 14:59:44 -0700

    remove dockerfile. add es

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.8.2-alpine
-RUN apk add gcc python-dev linux-headers
-
-WORKDIR /usr/src/app
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-COPY . .
-CMD [ "python" , "./garage.py" ]
--- garage.py
+++ garage.py
@@ -2,10 +2,11 @@
 from flask import Flask, json, request, jsonify
 app = Flask(__name__)
 
+from elasticsearch import Elasticsearch
 import Adafruit_DHT
 import RPi.GPIO as GPIO
+import requests
 import time
-from bluepy.btle import Scanner
 
 
 '''
@@ -14,54 +15,42 @@ Pin assignments
 RELAY = 21
 TEMP = 16
 
-'''
-ALlowed btle MAC addresses
-'''
-TILE_MACS = ['f2:0b:85:9a:76:ef']
-SIGNAL_STR = -90
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAY, GPIO.OUT)
 
 temp_sensor = Adafruit_DHT.DHT11
+
+es = Elasticsearch("192.168.1.111:9200")
+
+def _post_to_es(doc):
+    res = es.index(index="weather", body=doc)
+    return res
+    
+    
 
 def get_temp_humidity():
     humidity = None
     temp = None
     while (humidity is None and temp is None ):
         humidity, temp = Adafruit_DHT.read_retry(temp_sensor, TEMP)
-        return { "humidity": humidity, "temp" : temp}
+        return { "humidity": humidity, "temp" : temp,
+                 "timestamp": int(time.time()),
+                 "location": "garage" }
 
-
-def scan_for_tile():
-    scanner = Scanner()
-    devices = scanner.scan(3.0)
-    if not devices:
-        return (True, 0)
-    for dev in devices:
-        print(dev.addr)
-        if dev.addr in TILE_MACS and dev.rssi > SIGNAL_STR:
-            return (True, dev.rssi)
-    return (True, 0)
 
 
 @app.route('/garage')
 def open():
-    stats = {}
-    status,rssi = scan_for_tile()
-    stats['signal_strength'] = rssi
-    print(status)
-    if status:
-        GPIO.output(RELAY, True)
-        time.sleep(.5)
-        GPIO.output(RELAY, False)
-        stats['action'] = status
-    else:
-        stats['action'] = status
+    stats = dict()
+    GPIO.output(RELAY, True)
+    time.sleep(.5)
+    GPIO.output(RELAY, False)
     stats.update(get_temp_humidity())
+    _post_to_es(stats)
     return jsonify(stats)    
     
 if __name__ == "__main__":
     run_options = { "host":"0.0.0.0",
-                   "port": 80,
+                   "port": 8000,
                    "debug": False }
     app.run(**run_options)
